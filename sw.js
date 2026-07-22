8b23f08c06ea2350251cbefdf06982ad7175876e
BillyTheBot92 <only.game92700@gmail.com>
2026-07-22 13:18:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,47 +1,5 @@
-var CACHE_NAME = 'home-reader-v1.6.1';
-var ASSETS = [
-  './',
-  'home-reader.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png',
-  'libarchive.js',
-  'libarchive-worker.js',
-  'libarchive.wasm'
-];
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(ASSETS);
-    }).then(function() {
-      return self.skipWaiting();
-    })
-  );
-});
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(names) {
-      return Promise.all(
-        names.filter(function(n) { return n !== CACHE_NAME; })
-          .map(function(n) { return caches.delete(n); })
-      );
-    }).then(function() {
-      self.clients.matchAll().then(function(clients) {
-        clients.forEach(function(client) {
-          client.postMessage({ type: 'SW_UPDATED' });
-        });
-      });
-      return self.clients.claim();
-    })
-  );
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request);
-    })
-  );
-});
+var CACHE='home-reader-v2.0.0';
+var FILES=['./','/','index.html','home-reader.html','manifest.json','libarchive.js','libarchive-worker.js','libarchive.wasm'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES)}).then(function(){return self.skipWaiting()}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}).then(function(){self.clients.matchAll().then(function(cls){cls.forEach(function(c){c.postMessage({type:'SW_UPDATED'})})});return self.clients.claim()}))});
+self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;var url=new URL(e.request.url);if(url.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request).then(function(nr){if(nr&&nr.status===200&&nr.type==='basic'){var rc=nr.clone();caches.open(CACHE).then(function(c){c.put(e.request,rc)})}return nr})}))});
